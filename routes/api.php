73ff24b0c0fcaf2c511b3994c0b2b089a51a689e
Farrel Putra <70509726+thundergaming349@users.noreply.github.com>
2026-05-12 22:01:17 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function () {
-    $user = User::query();
+    $user = User::get();
     return response()->json([
         'user' => $user
     ]);
